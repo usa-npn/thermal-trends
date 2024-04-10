3706c8f81ce5b3e07388ebd74e48820506fe2bb4
Eric Scott <scottericr@gmail.com>
2024-04-10 14:48:39 -0700

    change color palette for DOY plots

--- R/plot_doy.R
+++ R/plot_doy.R
@@ -1,8 +1,11 @@
 plot_doy <- function(gdd_stack, gdd_threshold) {
   p <- ggplot() +
     geom_spatraster(data = gdd_stack) +
-    scale_fill_viridis_c(na.value = "transparent", direction = -1) +
-    facet_wrap(~lyr) +
+    scale_fill_viridis_c(
+      na.value = "transparent", option = "B",
+      direction = -1, #earlier DOY = hotter color
+      end = 0.9 #don't use the lightest yellow—hard to see on white background
+    ) +    facet_wrap(~lyr) +
     labs(
       title = glue::glue("Days to reach > {gdd_threshold} GDD"),
       fill = "DOY"
