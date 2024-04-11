18cc0b3d5931bbe5024740c916ad2607d96174c0
Renata Diaz <renatadiaz@arizona.edu>
2024-04-11 11:21:49 -0700

    Only try to summarize normals for years present in stack. Allows the pipeline to run for year intervals other than 1991:2020.

--- R/summarize_normals.R
+++ R/summarize_normals.R
@@ -3,6 +3,10 @@
 # tar_load(gdd_doy_stack_50)
 
 summarize_normals <- function(stack, years = 1991:2020) {
+  
+  years <- as.character(years)
+  years <- years[ which(years %in% names(stack))]
+  
   stack[[as.character(years)]] |> 
     terra::app(function(x) c(mean = mean(x, na.rm = TRUE), sd = sd(x, na.rm = TRUE)))
 }
