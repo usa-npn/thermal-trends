d956d4e581e72af995ee24a0443ae8be30cc61cb
Eric Scott <scottericr@gmail.com>
2024-04-18 10:09:56 -0700

    use cloud-optimized gtiffs

--- R/write_tiff.R
+++ R/write_tiff.R
@@ -1,7 +1,7 @@
 write_tiff <- function(raster, filename, out_dir = "output/rasters") {
   out_path <- fs::path(out_dir, filename)
   fs::dir_create(out_dir)
-  terra::writeRaster(raster, filename = out_path, filetype = "GTiff", overwrite = TRUE)
+  terra::writeRaster(raster, filename = out_path, filetype = "COG", overwrite = TRUE)
   
   #return:
   out_path
