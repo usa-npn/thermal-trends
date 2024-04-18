a58d8a7ac2248840ac58828244f66a3688f900e9
Eric Scott <scottericr@gmail.com>
2024-04-18 11:21:44 -0700

    don't write aux file

--- R/write_tiff.R
+++ R/write_tiff.R
@@ -1,7 +1,8 @@
 write_tiff <- function(raster, filename, out_dir = "output/rasters") {
   out_path <- fs::path(out_dir, filename)
   fs::dir_create(out_dir)
-  terra::writeRaster(raster, filename = out_path, filetype = "COG", overwrite = TRUE)
+  terra::writeRaster(raster, filename = out_path, filetype = "COG", overwrite = TRUE,
+                     gdal = c("GDAL_PAM_ENABLED=NO")) #turn off creation of aux.xml file
   
   #return:
   out_path
