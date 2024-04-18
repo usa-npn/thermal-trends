e3678b2d38cbec82b83fa007265b9b43949646a9
Eric Scott <scottericr@gmail.com>
2024-04-18 13:23:22 -0700

    set compression to LZW.  Default in new GDAL, but not set on version on HPC

--- R/write_tiff.R
+++ R/write_tiff.R
@@ -2,7 +2,10 @@ write_tiff <- function(raster, filename, out_dir = "output/rasters") {
   out_path <- fs::path(out_dir, filename)
   fs::dir_create(out_dir)
   terra::writeRaster(raster, filename = out_path, filetype = "COG", overwrite = TRUE,
-                     gdal = c("GDAL_PAM_ENABLED=NO")) #turn off creation of aux.xml file
+                     gdal = c(
+                       "GDAL_PAM_ENABLED=NO", #turn off creation of aux.xml file, doesn't seem to work
+                       "COMPRESS=LZW" #this is default in GDAL >= 3.4, not set on older versions
+                     )) 
   
   #return:
   out_path
