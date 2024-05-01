4da0b04c02dc5af3b30337454be03c80a7f804c3
Eric Scott <scottericr@gmail.com>
2024-05-01 14:17:09 -0700

    GDAL_PAM_ENABLED option doesn't work

--- R/write_tiff.R
+++ R/write_tiff.R
@@ -3,7 +3,6 @@ write_tiff <- function(raster, filename, out_dir = "output/rasters") {
   fs::dir_create(out_dir)
   terra::writeRaster(raster, filename = out_path, filetype = "COG", overwrite = TRUE,
                      gdal = c(
-                       "GDAL_PAM_ENABLED=NO", #turn off creation of aux.xml file, doesn't seem to work
                        "COMPRESS=LZW" #this is default in GDAL >= 3.4, not set on older versions
                      )) 
   
