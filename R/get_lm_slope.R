99c67a60f10703fd4d4d6f8a304185929257baf5
Eric Scott <ericrscott@arizona.edu>
2024-07-01 16:25:17 +0000

    Only fit a line if half of the years have non-NA values (#31)

--- R/get_lm_slope.R
+++ R/get_lm_slope.R
@@ -1,12 +1,11 @@
-#takes a stack of yearly DOY to get to GDD and returns raster with a layer of slopes and a layer of fdr adjusted p-values
+#takes a stack of yearly DOY to get to GDD and returns raster with a layer of
+#slopes and a layer of fdr adjusted p-values
 get_lm_slope <- function(gdd_stack) {
   years <- as.integer(names(gdd_stack))
   getTrend <- function(x) {
-    #only attempt to fit a line if there are at least 7 non-NA values
-    #this seems not quite right.  NAs here are really infinity (or 365??) because it means that pixel never reached the GDD threshold in that year.
-    # if (sum(!is.na(x)) < 7) {
-    #could instead only fit slopes when all values are non NA with
-    if(any(is.na(x))) {
+    #only attempt to fit a line if at least half the years have non-NA values
+    #for a particular pixel
+    if (sum(is.finite(x)) >= length(years)/2) {
       c(slope = NA, p.val = NA)
     } else {
       m = lm(x ~ years)
