8dde9882bdbe529b0fea6ea94d621de260b6987d
Eric Scott <scottericr@gmail.com>
2024-07-03 09:33:08 -0700

    increase walltime

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=5gb          
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --partition=standard
 #SBATCH --account=theresam
 #SBATCH -o logs/%x_%j.out
