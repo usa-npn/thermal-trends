395547876a2c4494a181c745a6e85644b63edb83
Eric Scott <scottericr@gmail.com>
2024-08-16 17:02:13 -0700

    🤦‍♂️actually scale year in newdata

--- R/make_slope_newdata.R
+++ R/make_slope_newdata.R
@@ -2,7 +2,8 @@ make_slope_newdata <- function(rast, res_m) {
   rast |> 
     project(crs("ESRI:102010"), res = res_m, method = "near") |> 
     as.data.frame(xy = TRUE, na.rm = TRUE) |> 
-    tidyr::pivot_longer(c(-x,-y), names_to = "year_scaled", values_to = "DOY") |> 
-    dplyr::mutate(year_scaled = as.numeric(year_scaled)) |> 
-    select(-DOY)
+    tidyr::pivot_longer(c(-x,-y), names_to = "year", values_to = "DOY") |> 
+    dplyr::mutate(year = as.numeric(year)) |> 
+    dplyr::mutate(year_scaled = year - min(year, na.rm = TRUE)) |> 
+    select(x, y, year_scaled)
 }
