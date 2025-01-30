99f9c1a780b07d6b75e4f020eae8b26fe1ea4c2e
Eric R. Scott <ericrscott@arizona.edu>
2025-01-30 22:37:21 +0000

    remove unused functions

---
+++ R/plot_sd_doy.R
@@ -0,0 +1,25 @@
+plot_sd_doy <- function(raster, range = NULL) {
+  raster_name <- deparse(substitute(raster))
+  threshold <- stringr::str_extract(raster_name, "\\d+")
+  p <- ggplot2::ggplot() +
+    tidyterra::geom_spatraster(data = raster) +
+    ggplot2::scale_fill_viridis_c(na.value = "transparent") +
+    ggplot2::labs(
+      title = glue::glue("Standard deviation in DOY that {threshold} GDD is reached"),
+      fill = "±days",
+      x = "",
+      y = ""
+    ) +
+    # coord_sf(crs = "ESRI:102010") +
+    ggplot2::theme_minimal()
+
+  ggplot2::ggsave(
+    filename = glue::glue("sd_{threshold}.png"),
+    plot = p, 
+    path = "output/gams/",
+    bg = "white",
+    width = 7,
+    height = 5
+  )
+}
+
