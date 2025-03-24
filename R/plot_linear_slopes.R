35a72c642e697d63fbae2e41dacdfef0b07434ba
Eric R. Scott <ericrscott@arizona.edu>
2025-03-24 17:58:43 +0000

    show ≥ and/or ≤ when oob_squish truncates scale

--- R/plot_linear_slopes.R
+++ R/plot_linear_slopes.R
@@ -8,16 +8,23 @@
 
 # dots <- rlang::dots_list(linear_slopes_50, linear_slopes_650, linear_slopes_1250, linear_slopes_1950, linear_slopes_2500, .named = TRUE)
 
-
+#' @param ... linear_slopes_{threshold} targets
+#' @param limits the limits used for the fill scale.  Use `NA` for the lower, 
+#'   upper, or both limits to use the range of the data.  Specify as numeric to 
+#'   override.
+#' @param use_percentile_lims ignore any input for `limits` and instead use 
+#'   limits that capture 95% of the DOY values.
 plot_linear_slopes <- function(..., roi, limits = c(NA, NA), use_percentile_lims = TRUE) {
   dots <- rlang::dots_list(..., .named = TRUE)
   thresholds <- stringr::str_extract(names(dots), "\\d+")
-  stack <- rast(dots)
+  stack <- terra::rast(dots)
   names(stack) <- thresholds
+  range <- range(terra::values(stack), na.rm = TRUE)
+  limits <- dplyr::coalesce(limits, range)
 
   if(use_percentile_lims) {
     #what if we want to use (global) 95%ile for limits?
-    limits <- stack |> values() |> quantile(probs = c(0.025, 0.975), na.rm = TRUE)
+    limits <- stack |> terra::values() |> quantile(probs = c(0.025, 0.975), na.rm = TRUE)
     # TODO:
     # hmm, this emphasizes the negative slopes more than positive because negative
     # slopes are more common over all thresholds.  Maybe better to do this on a 
@@ -32,7 +39,13 @@ plot_linear_slopes <- function(..., roi, limits = c(NA, NA), use_percentile_lims
       na.value = "transparent", 
       rev = TRUE, 
       limits = limits,
-      oob = scales::oob_squish 
+      oob = scales::oob_squish,
+      breaks = breaks_limits(
+        n = 5,
+        min = !is.na(limits[1]),
+        max = !is.na(limits[2]),
+        tol = 0.15
+      )
     ) +
     labs(
       fill = "Linear slope (DOY/yr)"
