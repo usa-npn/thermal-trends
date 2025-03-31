fcde21525aa463a45c22f0519c7338b508ab19d5
Eric R. Scott <ericrscott@arizona.edu>
2025-03-31 16:36:58 +0000

    better color scales (#40)

--- R/breaks_limits.R
+++ R/breaks_limits.R
@@ -1,10 +1,16 @@
-breaks_limits <- function (n = 5, tol = 0.1, min = TRUE, max = TRUE, digits = 1, ...) 
-{
+breaks_limits <- function(
+  n = 5,
+  tol = 0.1,
+  min = TRUE,
+  max = TRUE,
+  digits = 1,
+  ...
+) {
   n_default <- n
   scales:::force_all(n, tol, min, max, ...)
   function(x, n = n_default) {
     breaks <- pretty(x, n, ...)
-    
+
     #force limits to be included and remove breaks outside of limits
     if (isTRUE(min)) {
       breaks <- c(x[1], breaks)
@@ -13,15 +19,18 @@ breaks_limits <- function (n = 5, tol = 0.1, min = TRUE, max = TRUE, digits = 1,
       breaks <- c(x[2], breaks)
     }
     breaks <- unique(sort(breaks))
-    breaks <- breaks[breaks>=x[1] & breaks<=x[2]]
-    
+    breaks <- breaks[breaks >= x[1] & breaks <= x[2]]
+
     #remove breaks too close to limits that they are likely to overlap
     scl_br <- (breaks - min(breaks)) / diff(range(breaks)) #or diff(x)
     if (isTRUE(min) & abs(scl_br[1] - scl_br[2]) < tol) {
       breaks <- breaks[-2]
     }
-    if (isTRUE(max) & abs(scl_br[length(scl_br)] - scl_br[length(scl_br) - 1]) < tol) {
-      breaks <- breaks[-(length(breaks)-1)]
+    if (
+      isTRUE(max) &
+        abs(scl_br[length(scl_br)] - scl_br[length(scl_br) - 1]) < tol
+    ) {
+      breaks <- breaks[-(length(breaks) - 1)]
     }
     labels <- as.character(round(breaks, digits = digits))
     if (isTRUE(min)) {
--- R/plot_linear_slopes.R
+++ R/plot_linear_slopes.R
@@ -1,59 +1,73 @@
 # library(ggplot2)
 # library(tidyterra)
 # library(targets)
-# library(colorspace)
+# library(cols4all)
 # tar_load(starts_with("linear_slopes"))
 # tar_load(roi)
 
-# dots <- rlang::dots_list(linear_slopes_50, linear_slopes_650, linear_slopes_1250, linear_slopes_1950, linear_slopes_2500, .named = TRUE)
+# dots <- rlang::dots_list(
+#   linear_slopes_50,
+#   linear_slopes_650,
+#   linear_slopes_1250,
+#   linear_slopes_1950,
+#   linear_slopes_2500,
+#   .named = TRUE
+# )
 
 #' Plot pixel-wise linear slopes
-#' 
-#' Currently plots each threshold on it's own scale with the limits of the scale 
+#'
+#' Currently plots each threshold on it's own scale with the limits of the scale
 #' encompasing 99% of the data points with more extreme values being lumped in
 #' with the limits
-#' 
+#'
 #' @param ... linear_slopes_{threshold} targets
 #' @param use_percentile_lims use limits that capture 99% of the DOY values.
-#' 
+#'
 plot_linear_slopes <- function(..., roi, use_percentile_lims = TRUE) {
   dots <- rlang::dots_list(..., .named = TRUE)
   thresholds <- stringr::str_extract(names(dots), "\\d+")
   stack <- terra::rast(dots)
   names(stack) <- thresholds
-  # range <- range(terra::values(stack), na.rm = TRUE)
-  # limits <- dplyr::coalesce(limits, range)
-  
-  p_list <- purrr::map2(dots, thresholds, \(raster, threshold) {
-    if(use_percentile_lims) {
-      limits <- raster |> terra::values() |> quantile(probs = c(0.005, 0.995), na.rm = TRUE)
-    }
-  
+
+  limits <- c(NA, NA)
+  if (use_percentile_lims) {
+    limits <- stack |>
+      terra::values() |>
+      quantile(probs = c(0.005, 0.995), na.rm = TRUE)
+  }
+
+  p <-
     ggplot() +
-      # facet_wrap(vars(lyr)) +
-      geom_spatvector(data = roi, fill = "white") +
-      geom_spatraster(data = raster) +
-      scale_fill_continuous_diverging(
-        palette = "Blue-Red 3",
-        na.value = "transparent", 
-        rev = TRUE, 
-        limits = limits,
-        oob = scales::oob_squish,
-        breaks = breaks_limits(
-          n = 5,
-          min = !is.na(limits[1]),
-          max = !is.na(limits[2]),
-          tol = 0.15
-        )
-      ) +
-    # scale_fill_binned_diverging(
-    #   palette = "Blue-Red 3",
-    #   na.value = "transparent", 
-    #   rev = TRUE,
-    #   n.breaks = 7
+    facet_wrap(vars(lyr)) +
+    geom_spatvector(data = roi, fill = "white") +
+    geom_spatraster(data = stack) +
+    # cols4all::scale_fill_continuous_c4a_div(
+    #   palette = "bu_br_div",
+    #   na.value = "transparent",
+    #   reverse = TRUE,
+    #   limits = limits,
+    #   oob = scales::oob_squish,
+    #   breaks = breaks_limits(
+    #     n = 5,
+    #     min = !is.na(limits[1]),
+    #     max = !is.na(limits[2]),
+    #     tol = 0.15
+    #   )
     # ) +
+    colorspace::scale_fill_continuous_diverging(
+      palette = "Purple-Brown",
+      rev = TRUE,
+      na.value = "transparent",
+      limits = limits,
+      oob = scales::oob_squish,
+      breaks = breaks_limits(
+        n = 5,
+        min = !is.na(limits[1]),
+        max = !is.na(limits[2]),
+        tol = 0.15
+      )
+    ) +
     labs(
-      title = glue::glue("{threshold} GDD"),
       fill = "Linear slope (DOY/yr)"
     ) +
     # coord_sf(crs = "ESRI:102010") +
@@ -64,10 +78,7 @@ plot_linear_slopes <- function(..., roi, use_percentile_lims = TRUE) {
       legend.key.width = unit(0.5, "inches"),
       legend.key.height = unit(0.2, "inches")
     )
-    # p
-  })
-  
-  p <- patchwork::wrap_plots(p_list)
+  # p
 
   ggplot2::ggsave(
     filename = "linear_slopes.png",
@@ -84,7 +95,6 @@ plot_linear_slopes <- function(..., roi, use_percentile_lims = TRUE) {
 # plot_linear_slopes(linear_slopes_1250, linear_slope_limits)
 # plot_linear_slopes(linear_slopes_1950, linear_slope_limits)
 # plot_linear_slopes(linear_slopes_2500, linear_slope_limits)
-
 
 # #global range
 # plot_linear_slopes(linear_slopes_1950, linear_slope_limits)
