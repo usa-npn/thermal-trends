40ae61baec6061e3d44f5d65229bb6ee09eaa851
Eric R. Scott <ericrscott@arizona.edu>
2025-03-31 16:37:45 +0000

    require 10yrs of data (#40)

--- R/calc_linear_slopes.R
+++ R/calc_linear_slopes.R
@@ -4,12 +4,13 @@ calc_linear_slopes <- function(stack) {
   #scale
   year_scaled <- year - min(year, na.rm = TRUE)
   slopes <- terra::app(stack, \(x) {
-      if (sum(is.finite(x)) < 22) { #don't bother if there's fewer than half non-NA years
-        NA
-      } else {
-        m = lm(x ~ year_scaled)
-        coef(m)[2]
-      }
+    if (sum(is.finite(x)) < 10) {
+      #don't bother if there's fewer than 10 non-NA years
+      NA
+    } else {
+      m = lm(x ~ year_scaled)
+      coef(m)[2]
+    }
   })
   names(slopes) <- "slope"
   #return
