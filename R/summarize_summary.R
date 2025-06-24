d10a1da94177273925ba27f913f0c6fbc27cbe7b
Eric R. Scott <ericrscott@arizona.edu>
2025-06-24 20:55:18 +0000

    reorder outputs

--- R/summarize_summary.R
+++ R/summarize_summary.R
@@ -8,8 +8,8 @@ summarize_summary <- function(doy_summary) {
     as_tibble(na.rm = TRUE) |>
     summarize(
       doy_min = min(min),
-      doy_max = max(max),
       doy_mean = mean(mean),
+      doy_max = max(max),
       doy_sd_min = min(sd),
       doy_sd_mean = mean(sd),
       doy_sd_max = max(sd),
@@ -19,5 +19,3 @@ summarize_summary <- function(doy_summary) {
     ) |>
     mutate(threshold = threshold, .before = doy_min)
 }
-
-
